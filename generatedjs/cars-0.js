a86b11fba95b9401fdcae79364457dd5ddf54a23
iasmarket <eloabder@gmail.com>
2025-08-15 21:47:59 +0100

    Mise à jour generatedjs/cars-0.js

---
+++ generatedjs/cars-0.js
@@ -0,0 +1,15 @@
+window.cars0 = [
+    {
+        "Ref": "P2P-Cars20250815114512075",
+        "marque": "BYD",
+        "modele": "G3",
+        "annee": "2010",
+        "kilometrage": 777777,
+        "carburant": "Essence",
+        "puissance": 7,
+        "images": [
+            "datatof\\1755258312057-578493572.jpeg;datatof\\1755258312058-901634705.jpeg;datatof\\1755258312060-73866293.jpeg;datatof\\1755258312061-743543302.jpeg;datatof\\1755258312062-342831133.jpeg;datatof\\1755258312063-821992664.jpeg"
+        ],
+        "description": "HHHHHHHHHHHHHHH YRE HEERH E"
+    }
+];
