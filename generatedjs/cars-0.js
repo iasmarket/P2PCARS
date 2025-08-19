d0a2ebe931c5a17a4a7d1d99162009c2bcaf0440
iasmarket <eloabder@gmail.com>
2025-08-19 23:19:53 +0100

    Mise à jour generatedjs/cars-0.js

---
+++ generatedjs/cars-0.js
@@ -0,0 +1,19 @@
+window.cars0 = [
+    {
+        "Ref": "P2P-Cars20250818144610717",
+        "marque": "Aptera",
+        "modele": "3",
+        "annee": "2012",
+        "kilometrage": 22222,
+        "carburant": "Essence",
+        "puissance": 32,
+        "images": [
+            "datatof/1755528370705-857592215.jpeg",
+            "datatof/1755528370705-715501994.jpeg",
+            "datatof/1755528370707-633541442.jpeg",
+            "datatof/1755528370708-903436178.jpeg",
+            "datatof/1755528370708-747936127.jpeg"
+        ],
+        "description": "zdqfq qf fqf"
+    }
+];
