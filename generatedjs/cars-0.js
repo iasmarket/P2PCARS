1b062c5d5412eb1c40c00a1c41ff9aac3a44422b
iasmarket <eloabder@gmail.com>
2025-08-19 22:30:42 +0100

    Mise à jour generatedjs/cars-0.js

---
+++ generatedjs/cars-0.js
@@ -0,0 +1,15 @@
+window.cars0 = [
+    {
+        "Ref": "P2P-Cars20250819213015012",
+        "marque": "Arrinera",
+        "modele": "Hussarya",
+        "annee": "2020",
+        "kilometrage": 52,
+        "carburant": "Essence",
+        "puissance": 56,
+        "images": [
+            "datatof/1755639014719-169817739.jpg"
+        ],
+        "description": "اتال"
+    }
+];
