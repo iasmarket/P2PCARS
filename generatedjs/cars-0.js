9dee86d1198c5cb879dade17867c2fc964d7c7d8
iasmarket <eloabder@gmail.com>
2025-08-21 12:27:31 +0100

    Mise à jour generatedjs/cars-0.js

---
+++ generatedjs/cars-0.js
@@ -0,0 +1,20 @@
+window.cars0 = [
+    {
+        "Ref": "P2P-Cars20250820152956696",
+        "marque": "Ford",
+        "modele": "C-Max",
+        "annee": "2013",
+        "kilometrage": 199000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1755703796683-39327387.jpeg",
+            "datatof/1755703796684-140073927.jpeg",
+            "datatof/1755703796685-408346803.jpeg",
+            "datatof/1755703796686-10230814.jpeg"
+        ],
+        "description": "PAS MAL de Lqliaa",
+        "ville": "Lqliaa",
+        "prix": "130000"
+    }
+];
