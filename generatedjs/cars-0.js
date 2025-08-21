308e276b76e2d3ce3caa399ecf35e7a6551c2b80
iasmarket <eloabder@gmail.com>
2025-08-21 12:33:24 +0100

    Update cars-0.js

--- generatedjs/cars-0.js
+++ generatedjs/cars-0.js
@@ -1,6 +1,6 @@
 window.cars0 = [
     {
-        "Ref": "P2P-Cars20250820152956696",
+        "Ref": "P2P-Cars20250821120100001",
         "marque": "Ford",
         "modele": "C-Max",
         "annee": "2013",
@@ -9,12 +9,74 @@ window.cars0 = [
         "puissance": 6,
         "images": [
             "datatof/1755703796683-39327387.jpeg",
-            "datatof/1755703796684-140073927.jpeg",
-            "datatof/1755703796685-408346803.jpeg",
-            "datatof/1755703796686-10230814.jpeg"
+            "datatof/1755703796684-140073927.jpeg"
         ],
         "description": "PAS MAL de Lqliaa",
         "ville": "Lqliaa",
         "prix": "130000"
+    },
+    {
+        "Ref": "P2P-Cars20250821120100002",
+        "marque": "Toyota",
+        "modele": "Corolla",
+        "annee": "2015",
+        "kilometrage": 150000,
+        "carburant": "Essence",
+        "puissance": 7,
+        "images": [
+            "datatof/1755528370705-715501994.jpeg",
+            "datatof/1755528370707-633541442.jpeg"
+        ],
+        "description": "Très bon état",
+        "ville": "Casablanca",
+        "prix": "145000"
+    },
+    {
+        "Ref": "P2P-Cars20250821120100003",
+        "marque": "Renault",
+        "modele": "Clio",
+        "annee": "2018",
+        "kilometrage": 90000,
+        "carburant": "Diesel",
+        "puissance": 5,
+        "images": [
+            "datatof/1755699161506-841283806.jpeg",
+            "datatof/1755699161507-623336989.jpeg"
+        ],
+        "description": "Idéale pour la ville",
+        "ville": "Rabat",
+        "prix": "125000"
+    },
+    {
+        "Ref": "P2P-Cars20250821120100004",
+        "marque": "Peugeot",
+        "modele": "208",
+        "annee": "2017",
+        "kilometrage": 120000,
+        "carburant": "Essence",
+        "puissance": 5,
+        "images": [
+            "datatof/1755258312057-578493572.jpeg",
+            "datatof/1755258312058-901634705.jpeg"
+        ],
+        "description": "Bonne voiture familiale",
+        "ville": "Marrakech",
+        "prix": "138000"
+    },
+    {
+        "Ref": "P2P-Cars20250821120100005",
+        "marque": "Honda",
+        "modele": "Civic",
+        "annee": "2016",
+        "kilometrage": 110000,
+        "carburant": "Essence",
+        "puissance": 6,
+        "images": [
+            "datatof/1755528370708-747936127.jpeg",
+            "datatof/1755528370708-903436178.jpeg"
+        ],
+        "description": "Excellent état mécanique",
+        "ville": "Fès",
+        "prix": "142000"
     }
 ];
