5badbd9672d6690f2d1a6ebf5067d10cc6c52a78
iasmarket <eloabder@gmail.com>
2025-08-31 10:10:51 +0100

    Mise à jour generatedjs/cars-0.js

---
+++ generatedjs/cars-0.js
@@ -0,0 +1,78 @@
+window.cars0 = [
+    {
+        "Ref": "P2P-Cars202508261940594865",
+        "marque": "Opel",
+        "modele": "Zafira",
+        "annee": "2021",
+        "kilometrage": 190000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1756237259422-297659654.jpeg",
+            "datatof/1756237259422-582955649.jpeg",
+            "datatof/1756237259424-302693317.jpeg",
+            "datatof/1756237259426-879870442.jpeg",
+            "datatof/1756237259434-203594398.jpg"
+        ],
+        "description": "EXEMPLE",
+        "ville": "",
+        "prix": "190000"
+    },
+    {
+        "Ref": "P2P-Cars202508261940594864",
+        "marque": "Renault",
+        "modele": "Scénic",
+        "annee": "2021",
+        "kilometrage": 190000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1756237259422-297659654.jpeg",
+            "datatof/1756237259422-582955649.jpeg",
+            "datatof/1756237259424-302693317.jpeg",
+            "datatof/1756237259426-879870442.jpeg",
+            "datatof/1756237259434-203594398.jpg"
+        ],
+        "description": "EXEMPLE",
+        "ville": "",
+        "prix": "190000"
+    },
+    {
+        "Ref": "P2P-Cars20250826194059483",
+        "marque": "Ford",
+        "modele": "C-Max",
+        "annee": "2021",
+        "kilometrage": 190000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1756237259422-297659654.jpeg",
+            "datatof/1756237259422-582955649.jpeg",
+            "datatof/1756237259424-302693317.jpeg",
+            "datatof/1756237259426-879870442.jpeg",
+            "datatof/1756237259434-203594398.jpg"
+        ],
+        "description": "EXEMPLE",
+        "ville": "",
+        "prix": "190000"
+    },
+    {
+        "Ref": "P2P-Cars20250826194059486",
+        "marque": "Toyota",
+        "modele": "Verso",
+        "annee": "2021",
+        "kilometrage": 190000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1756237259422-297659654.jpeg",
+            "datatof/1756237259422-582955649.jpeg",
+            "datatof/1756237259424-302693317.jpeg",
+            "datatof/1756237259426-879870442.jpeg",
+            "datatof/1756237259434-203594398.jpg"
+        ],
+        "description": "EXEMPLE",
+        "ville": "",
+        "prix": "190000"
+    }
+];
