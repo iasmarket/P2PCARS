c97797d9b8de11006badc1d882575121a2139f6c
iasmarket <eloabder@gmail.com>
2025-09-18 12:02:05 +0100

    Mise à jour generatedjs/cars-0.js

---
+++ generatedjs/cars-0.js
@@ -0,0 +1,39 @@
+window.cars0 = [
+    {
+        "Ref": "P2P-Cars20250918105723779",
+        "marque": "Dacia",
+        "modele": "Sandero",
+        "annee": "2019",
+        "kilometrage": 586556,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1758193043467-195799702.jpg"
+        ],
+        "description": "Très bon état  de Casablanca",
+        "ville": "Casablanca",
+        "prix": "80000"
+    },
+    {
+        "Ref": "P2P-Cars20250913130934224",
+        "marque": "Opel",
+        "modele": "Corsa",
+        "annee": "2022",
+        "kilometrage": 112000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1757768974034-367301222.jpg",
+            "datatof/1757768974081-714090552.jpg",
+            "datatof/1757768974108-704733057.jpg",
+            "datatof/1757768974127-214947249.jpg",
+            "datatof/1757768974147-932745389.jpg",
+            "datatof/1757768974160-628502902.jpg",
+            "datatof/1757768974200-645000090.jpg",
+            "datatof/1757768974205-325122516.jpg"
+        ],
+        "description": "Opel corsa GS LINE PLUS , PREMIÈRE MAIN. de Agadir",
+        "ville": "Agadir",
+        "prix": "165000"
+    }
+];
