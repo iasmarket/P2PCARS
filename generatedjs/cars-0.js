25e48f5032a790f7b7e3b6824fd71732ba6772a8
iasmarket <eloabder@gmail.com>
2025-09-19 17:16:10 +0100

    Mise à jour generatedjs/cars-0.js

---
+++ generatedjs/cars-0.js
@@ -0,0 +1,218 @@
+window.cars0 = [
+    {
+        "Ref": "P2P-Cars20250918124305947",
+        "marque": "Nissan",
+        "modele": "X-Trail",
+        "annee": "2009",
+        "kilometrage": 400000,
+        "carburant": "Diesel",
+        "puissance": 9,
+        "images": [
+            "datatof/1758199385811-692542537.jpg",
+            "datatof/1758199385866-739335454.jpg",
+            "datatof/1758199385896-751032805.jpg",
+            "datatof/1758199385913-583502639.jpg"
+        ],
+        "description": "Panne embrayage,2 vignettes impayés \r\r\n0626191892 de Agadir",
+        "ville": "Agadir",
+        "prix": "58000"
+    },
+    {
+        "Ref": "P2P-Cars20250918093258271",
+        "marque": "Brilliance",
+        "modele": "M1",
+        "annee": "2023",
+        "kilometrage": 77,
+        "carburant": "Electrique",
+        "puissance": 77777,
+        "images": [
+            "datatof/1758187978220-707273936.jpeg",
+            "datatof/1758187978225-242612433.jpeg",
+            "datatof/1758187978228-467727540.jpeg",
+            "datatof/1758187978231-293635966.jpeg"
+        ],
+        "description": "uyrturyuytu de Chtouka",
+        "ville": "Chtouka",
+        "prix": "77"
+    },
+    {
+        "Ref": "P2P-Cars20250918092018137",
+        "marque": "Alfa Romeo",
+        "modele": "156",
+        "annee": "2011",
+        "kilometrage": 44444,
+        "carburant": "Diesel",
+        "puissance": 4,
+        "images": [
+            "datatof/1758187218089-187620591.jpeg",
+            "datatof/1758187218097-545239914.jpeg",
+            "datatof/1758187218099-279801963.jpeg",
+            "datatof/1758187218100-522271667.jpeg"
+        ],
+        "description": "44 de Aïn El Aouda",
+        "ville": "Aïn El Aouda",
+        "prix": "4"
+    },
+    {
+        "Ref": "P2P-Cars20250916003010659",
+        "marque": "Aixam",
+        "modele": "Coupé",
+        "annee": "2013",
+        "kilometrage": 542452,
+        "carburant": "GPL",
+        "puissance": 74542,
+        "images": [
+            "datatof/1757982610632-335652063.jpeg"
+        ],
+        "description": "tytrtr de Aïn Cheggag",
+        "ville": "Aïn Cheggag",
+        "prix": "54254"
+    },
+    {
+        "Ref": "P2P-Cars20250915230317880",
+        "marque": "Peugeot",
+        "modele": "306",
+        "annee": "2009",
+        "kilometrage": 200000,
+        "carburant": "ديزل",
+        "puissance": 6,
+        "images": [
+            "datatof/1757977396866-504182311.jpeg",
+            "datatof/1757977397239-622336446.jpeg",
+            "datatof/1757977397426-400524076.jpeg",
+            "datatof/1757977397611-267580836.jpeg"
+        ],
+        "description": "fdgdgf de undefined",
+        "ville": "",
+        "prix": "50000"
+    },
+    {
+        "Ref": "P2P-Cars20250913130934224",
+        "marque": "Opel",
+        "modele": "Corsa",
+        "annee": "2022",
+        "kilometrage": 112000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1757768974034-367301222.jpg",
+            "datatof/1757768974081-714090552.jpg",
+            "datatof/1757768974108-704733057.jpg",
+            "datatof/1757768974127-214947249.jpg",
+            "datatof/1757768974147-932745389.jpg",
+            "datatof/1757768974160-628502902.jpg",
+            "datatof/1757768974200-645000090.jpg",
+            "datatof/1757768974205-325122516.jpg"
+        ],
+        "description": "Opel corsa GS LINE PLUS , PREMIÈRE MAIN. de Agadir",
+        "ville": "Agadir",
+        "prix": "165000"
+    },
+    {
+        "Ref": "P2P-Cars20250830211817334",
+        "marque": "Aixam",
+        "modele": "Coupé",
+        "annee": "2023",
+        "kilometrage": 123213,
+        "carburant": "Essence",
+        "puissance": 13121,
+        "images": [
+            "datatof/1756588697321-280626099.jpeg"
+        ],
+        "description": "edfsgfdgd de Ahouli",
+        "ville": "Ahouli",
+        "prix": "32321321"
+    },
+    {
+        "Ref": "P2P-Cars20250830161828768",
+        "marque": "Ford",
+        "modele": "C-Max",
+        "annee": "2013",
+        "kilometrage": 190000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1756570708709-338866248.jpg",
+            "datatof/1756570708721-853025779.jpg"
+        ],
+        "description": "propre et en bon etat de undefined",
+        "ville": "",
+        "prix": "130000"
+    },
+    {
+        "Ref": "P2P-Cars202508261940594866",
+        "marque": "Kia",
+        "modele": "Carens",
+        "annee": "2021",
+        "kilometrage": 190000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1756237259422-297659654.jpeg",
+            "datatof/1756237259422-582955649.jpeg",
+            "datatof/1756237259424-302693317.jpeg",
+            "datatof/1756237259426-879870442.jpeg",
+            "datatof/1756237259434-203594398.jpg"
+        ],
+        "description": "EXEMPLE",
+        "ville": "",
+        "prix": "190000"
+    },
+    {
+        "Ref": "P2P-Cars202508261940594865",
+        "marque": "Opel",
+        "modele": "Zafira",
+        "annee": "2021",
+        "kilometrage": 190000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1756237259422-297659654.jpeg",
+            "datatof/1756237259422-582955649.jpeg",
+            "datatof/1756237259424-302693317.jpeg",
+            "datatof/1756237259426-879870442.jpeg",
+            "datatof/1756237259434-203594398.jpg"
+        ],
+        "description": "EXEMPLE",
+        "ville": "",
+        "prix": "190000"
+    },
+    {
+        "Ref": "P2P-Cars202508261940594864",
+        "marque": "Renault",
+        "modele": "Scénic",
+        "annee": "2021",
+        "kilometrage": 190000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1756237259422-297659654.jpeg",
+            "datatof/1756237259422-582955649.jpeg",
+            "datatof/1756237259424-302693317.jpeg",
+            "datatof/1756237259426-879870442.jpeg",
+            "datatof/1756237259434-203594398.jpg"
+        ],
+        "description": "EXEMPLE",
+        "ville": "",
+        "prix": "190000"
+    },
+    {
+        "Ref": "P2P-Cars20250826194059483",
+        "marque": "Ford",
+        "modele": "C-Max",
+        "annee": "2021",
+        "kilometrage": 190000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1756237259422-297659654.jpeg",
+            "datatof/1756237259422-582955649.jpeg",
+            "datatof/1756237259424-302693317.jpeg",
+            "datatof/1756237259426-879870442.jpeg",
+            "datatof/1756237259434-203594398.jpg"
+        ],
+        "description": "EXEMPLE",
+        "ville": "",
+        "prix": "190000"
+    }
+];
