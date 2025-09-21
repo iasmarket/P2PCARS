12e8e29f71fd2ad6ffeb5c682cfdd43430217bbf
iasmarket <eloabder@gmail.com>
2025-09-21 21:55:21 +0100

    Mise à jour generatedjs/cars-0.js

---
+++ generatedjs/cars-0.js
@@ -0,0 +1,42 @@
+window.cars0 = [
+    {
+        "Ref": "P2P-Cars20250918124305947",
+        "marque": "Nissan",
+        "modele": "X-Trail",
+        "annee": "2009",
+        "kilometrage": 400000,
+        "carburant": "Diesel",
+        "puissance": 9,
+        "images": [
+            "datatof/1758199385811-692542537.jpg",
+            "datatof/1758199385866-739335454.jpg",
+            "datatof/1758199385896-751032805.jpg",
+            "datatof/1758199385913-583502639.jpg"
+        ],
+        "description": "Panne embrayage,2 vignettes impayés \r\r\n0626191892 de Agadir",
+        "ville": "Agadir",
+        "prix": "58000"
+    },
+    {
+        "Ref": "P2P-Cars20250913130934224",
+        "marque": "Opel",
+        "modele": "Corsa",
+        "annee": "2022",
+        "kilometrage": 112000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1757768974034-367301222.jpg",
+            "datatof/1757768974081-714090552.jpg",
+            "datatof/1757768974108-704733057.jpg",
+            "datatof/1757768974127-214947249.jpg",
+            "datatof/1757768974147-932745389.jpg",
+            "datatof/1757768974160-628502902.jpg",
+            "datatof/1757768974200-645000090.jpg",
+            "datatof/1757768974205-325122516.jpg"
+        ],
+        "description": "Opel corsa GS LINE PLUS , PREMIÈRE MAIN. de Agadir",
+        "ville": "Agadir",
+        "prix": "165000"
+    }
+];
