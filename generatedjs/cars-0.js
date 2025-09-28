d5dcc43cad02beaa8113c4b83d1fd88e8522f5e1
iasmarket <eloabder@gmail.com>
2025-09-28 14:54:17 +0100

    Mise à jour generatedjs/cars-0.js

---
+++ generatedjs/cars-0.js
@@ -0,0 +1,45 @@
+window.cars0 = [
+    {
+        "Ref": "P2P-Cars20250928135338041",
+        "marque": "Peugeot",
+        "modele": "206",
+        "annee": "2005",
+        "kilometrage": 287000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1759067617706-480234430.jpg",
+            "datatof/1759067617763-746004193.jpg",
+            "datatof/1759067617798-499284832.jpg",
+            "datatof/1759067617825-597237033.jpg",
+            "datatof/1759067617866-773978845.jpg",
+            "datatof/1759067617914-342839960.jpg",
+            "datatof/1759067617944-15406200.jpg"
+        ],
+        "description": "👍 في خالة جيدة \r\nكسابية  de Biougra",
+        "ville": "Biougra",
+        "prix": "55000"
+    },
+    {
+        "Ref": "P2P-Cars20250913130934224",
+        "marque": "Opel",
+        "modele": "Corsa",
+        "annee": "2022",
+        "kilometrage": 112000,
+        "carburant": "Diesel",
+        "puissance": 6,
+        "images": [
+            "datatof/1757768974034-367301222.jpg",
+            "datatof/1757768974081-714090552.jpg",
+            "datatof/1757768974108-704733057.jpg",
+            "datatof/1757768974127-214947249.jpg",
+            "datatof/1757768974147-932745389.jpg",
+            "datatof/1757768974160-628502902.jpg",
+            "datatof/1757768974200-645000090.jpg",
+            "datatof/1757768974205-325122516.jpg"
+        ],
+        "description": "Opel corsa GS LINE PLUS , PREMIÈRE MAIN. de Agadir",
+        "ville": "Agadir",
+        "prix": "165000"
+    }
+];
